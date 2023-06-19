98d44239d35136e5383e1abb873510b89f7d0a86
cj0x39e <taozhi1028@gmail.com>
2023-06-19 17:28:51 +0800

    refactor

--- examples/vite-demo/src/main.ts
+++ examples/vite-demo/src/main.ts
@@ -6,12 +6,12 @@ import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
-import HomeVue from "./views/Home.vue";
+import Home from "./views/Home.vue";
 
 const routes = [
   {
     path: "/",
-    component: HomeVue,
+    component: Home,
     children: [
       { path: "one", component: () => import("./views/PageOne.vue") },
       { path: "two", component: () => import("./views/PageTwo.vue") },
